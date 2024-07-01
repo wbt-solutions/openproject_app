c51fffaf088d6c8e089699fe3c80c933d9a050e2
Simon Bullik <sbu@wbt-solutions.de>
2024-07-01 23:20:09 +0200

    Regenerate project

--- android/app/src/main/kotlin/de/wbtsolutions/openproject_app/MainActivity.kt
+++ android/app/src/main/kotlin/de/wbtsolutions/openproject_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package de.wbtsolutions.openproject_app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity: FlutterActivity()
